031a5e767ea8464c82d797a26ae2aed041f1b188
ZHEQIUSHUI <qiushui.zhe@gmail.com>
2023-09-07 16:08:37 +0800

    add dinov2 depth estimation

--- examples/libaxdl/src/ax_model_seg.hpp
+++ examples/libaxdl/src/ax_model_seg.hpp
@@ -25,3 +25,12 @@ protected:
     void draw_custom(int chn, axdl_results_t *results, float fontscale, int thickness) override;
 };
 REGISTER(MT_SEG_DINOV2, ax_model_dinov2)
+
+class ax_model_dinov2_depth : public ax_model_pphumseg
+{
+protected:
+    int post_process(axdl_image_t *pstFrame, axdl_bbox_t *crop_resize_box, axdl_results_t *results) override;
+    void draw_custom(cv::Mat &image, axdl_results_t *results, float fontscale, int thickness, int offset_x, int offset_y) override;
+    void draw_custom(int chn, axdl_results_t *results, float fontscale, int thickness) override;
+};
+REGISTER(MT_SEG_DINOV2_DEPTH, ax_model_dinov2_depth)
